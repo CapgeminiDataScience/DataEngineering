8471fe2559773a5d9e1f5d301e3803f32b198203
Rhianna Tomlinson <rhianna.a.tomlinson@capgemini.com>
2019-05-11 20:53:36 +0100

    Simplified main script

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import sys
+import os
+
 from utilities.application_helper import start_application, end_application
 from utilities.logging_manager import initialise_logging
 
 
-if __name__ == "__main__":
+def main():
+
+    print(os.path.dirname(__file__))
 
     start = start_application()
     logger = initialise_logging(__file__, __name__)
-
-    file = open("/tmp/test.txt", "w+")
-    file.write("Success")
-    file.close()
     logger.info("Success")
 
     end_application(start)
+
+
+if __name__ == "__main__":
+
+    main()
