631856e6f7d9fa71c85168c07f286ca84b74beac
Rhianna Tomlinson <rhianna.a.tomlinson@capgemini.com>
2019-05-11 20:56:46 +0100

    Added more information

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as file:
@@ -9,23 +10,29 @@ with open("test/requirements.txt") as file:
 setup (
 
     # Define project metadata
-    name = "template-project-python",
-    version = "0.1",
-    description = "Generic python project template",
-    author = "Capgemini",
-    license = "Commercial",
+    name         = "template-project-python",
+    version      = "0.1",
+    license      = "Commercial",
+    description  = "Generic python project template",
+    url          = "https://bitbucket.org/rhiannajayne/template-project-python/wiki/Home",
+    author       = "Rhianna Tomlinson (Capgemini)",
+    author_email = "rhianna.a.tomlinson@capgemini.com",
+    long_description = "This repository is designed to serve as a generic python template for Capgemini projects",
 
     # Install list of requirements
     install_requires = main_requirements,
 
-    # Include all directories within project with an init.py file
+    # Include all directories within project with an init file
     packages = find_packages(),
 
-    # Include additional files without a .py file ending, using the relevant package as the key
+    # Include additional files matching a given pattern, using the relevant package as the key
     package_data = {"": ["*.md", "*.rst"], "test": ["*.txt"]},
 
     # Specify a test class or package with associated requirements
     test_suite = "test",
-    test_require = test_requirements
+    test_require = test_requirements,
 
+    # Define the methods to be surfaced as part of you package
+    entry_points = {"console_scripts": ["template = main:main"]}
+    # entry_points = {'setuptools.installation': ['eggsecutable = main:main']}
 )
